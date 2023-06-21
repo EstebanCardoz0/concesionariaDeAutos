9773011f4ecc80b557e8bc554b4e5363d4fab78d
esteban cardozo <esteban.cardozo.wec@gmail.com>
2023-06-21 12:42:31 -0300

    21 jun

---
+++ Ejercicios_Java/src/main/java/com/mycompany/ejercicios_java/Ejercicios_Java.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.ejercicios_java;
+
+import IGU.Pantalla;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejercicios_Java {
+    
+    public static void main(String[] args) {
+        
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        
+        
+        
+        
+    }//final
+}
